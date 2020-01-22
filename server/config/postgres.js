0b949a3978512a3c2922c6f7dcb99d7594d7cbcb
Ian <fntcstudent@FNTCs-MacBook-Pro.local>
2020-01-22 13:42:34 -0800

    initial commit

---
+++ server/config/postgres.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+
+module.exports = (app) => {
+
+  return new Pool({
+    host: app.get('PG_HOST'),
+    user: app.get('PG_USER'),
+    password: app.get('PG_PASSWORD'),
+    database: app.get('PG_DB'),
+    idleTimeoutMillis: 30000,
+    connnectonTimeoutMillis: 2000
+  });
+};
